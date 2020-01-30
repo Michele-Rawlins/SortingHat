bec5fc808e9e43157f246a2b25e098b090750ec4
Michele Rawlins <shellbop@att.net>
2020-01-29 20:11:29 -0600

    added bootstrap

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log("help");
+// console.log("help");
+const createStudent = [];
+
+const expelStudent = [];
+
+
+const printToDom = (divId, textToPrint)=> {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const duckPrinter = (ducks)=>{
+    let domString = "";
+for(let i =0; i < ducks.length; i++){
+    
+    domString += 
+
+
+    domString += 
+    domString += 
+    domString += 
+    domString += 
+    domString += 
+    domString += 
+    domString += 
+    domString += 
+// domString += `<h2 id = name>${ducks[i].name}</h2>`
+}
+printToDom('name',domString);
+};
+const chooseColor = (e) => {
+    console.log(e);
+    const buttonId = e.target.id;
+    const selectedDucks = [];
+    for(let i = 0; i < ducks.length; i++){
+        if(ducks[i].color === buttonId){
+            selectedDucks.push(ducks[i]);
+        }
+    }
+
+    duckPrinter(selectedDucks);
+};
+
+
+const events = () => {
+    document.getElementById('blue').addEventListener('click',chooseColor);
+    document.getElementById('red').addEventListener('click',chooseColor);
+    document.getElementById('yellow').addEventListener('click',chooseColor);
+}
+
+
